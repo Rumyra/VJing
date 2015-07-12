c65a272226ad8e30c1cd53d9e207c164600d2938
Rumyra <rumyra@gmail.com>
2015-07-12 23:42:33 +0100

    some work to get initial view, lights & video

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # VJing
 
 First VJing repo - getting some visuals in the browser, adding audio analysing functionlity and MIDI controlling.
+
+Sorry Chrome only - use case is running along a DJ, in a club, onto a projector, I think I can specify browser :)
--- public/app.js
+++ public/app.js
@@ -1,34 +1,59 @@
 
-function createMap(mapEl, mapLat, mapLon, zoom) {
-  zoom = typeof zoom !== 'undefined' ? zoom : 12;
-  var myOptions = {
-    zoom: zoom,
-    center: new google.maps.LatLng(mapLat, mapLon)
-  };
-  var mapObject = new google.maps.Map(mapEl, myOptions);
-  return mapObject;
+// ~~~~ To alter HTML & CSS tracks to music
+var analyseCss = function analyseCss(allElements, frequencyDataArray, analyserNode) {
+  // always poll this function
+  requestAnimationFrame(analyseCss);
+  //constantly getting feedback from data
+  analyserNode.getByteFrequencyData(frequencyDataArray);
+
+  var totalElements = allElements.length;
+
+  for (var i=0; i<totalElements; i++) {
+    //set light colours
+    var elementColour = i*10;
+    totalElements[i].style.backgroundColor = 'hsla('+elementColour+',  80%, 50%, 0.8)';
+    totalElements[i].style.borderColor = 'hsla('+elementColour+',  80%, 50%, 1)';
+    //flash on frequency
+    var freqDataKey = i*6;
+    if (frequencyDataArray[freqDataKey] > 160){
+      //start animation on element
+      totalElements[i].style.opacity = "1";
+    } else {
+      totalElements[i].style.opacity = "0.2";
+    }
+  }
 }
 
-// google maps calculate route function, modified from http://www.sitepoint.com/find-a-route-using-the-geolocation-and-the-google-maps-api/
-function calculateRoute(from, to, googleMap) {
-  var directionsService = new google.maps.DirectionsService();
-  var directionsRequest = {
-    origin: from,
-    destination: to,
-    travelMode: google.maps.DirectionsTravelMode.DRIVING,
-    unitSystem: google.maps.UnitSystem.METRIC
-  };
-  directionsService.route(
-    directionsRequest,
-    function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        new google.maps.DirectionsRenderer({
-          map: googleMap,
-          directions: response
-        });
+// ~~~ To change source of video tracks
+var changeVidSrc = function changeVidSrc(videoEl, newSrc) {
+  videoEl.src = newSrc;
+}
+
+// ~~~~ To mix two video tracks to music
+var mixVideos = function mixVideos(vidEl, frequencyDataArray, analyserNode) {
+  requestAnimationFrame(mixVideos);
+  //constantly getting feedback from data
+  analyserNode.getByteFrequencyData(frequencyDataArray);
+
+  for (var i=0; i<49; i++) {
+    var freqDataKey = i*8;
+    if (frequencyDataArray[freqDataKey] > 160){
+      if (i<10) {
+        vidEl.style.opacity = '1';
       } else {
-        console.warn('Unable to retrieve your route');
+        vidEl.style.opacity = '0';
       }
     }
-  );
+  }
+
 }
+
+var showVideo = function showVideo(vidEl, cssEl) {
+  vidEl.style.display === "block"
+  cssEl.style.display === "none"
+}
+var showCss = function showCss(vidEl, cssEl) {
+  cssEl.style.display === "block"
+  vidEl.style.display === "none"
+}
+
